6f8542f95985725bb0fa4d85c97fac7f2dcaa04f
Karan Singh <96336689+karanns19@users.noreply.github.com>
2022-04-07 16:15:28 +0530

    Add files via upload
    Source containing all the js and css files

---
+++ src/ELearning.js
@@ -0,0 +1,82 @@
+import styles from "./css/ELearning.module.css";
+
+export const ELearning = () => {
+  return (
+    <div className={styles.eLearningDiv}>
+      <div className={styles.rectangleDiv} />
+      <div className={styles.rectangleDiv1} />
+      <section className={styles.rectangleSection} />
+      <img className={styles.groupIcon} alt="" src="group.svg" />
+      <a className={styles.search}>Search</a>
+      <a className={styles.homeA}>Home</a>
+      <a className={styles.coursesA}>Courses</a>
+      <img className={styles.groupIcon1} alt="" src="group-2.svg" />
+      <div className={styles.rectangleDiv2} />
+      <div className={styles.loginDiv}>Login</div>
+      <h4 className={styles.coursesToGetYouStarted}>
+        Courses to get you started
+      </h4>
+      <h4 className={styles.advanceYourCareerWithNewS}>
+        Advance your career with new skill
+      </h4>
+      <h3 className={styles.designCoursesH3}>Design Courses</h3>
+      <img className={styles.rectangleIcon} alt="" src="rectangle-22@2x.png" />
+      <img className={styles.rectangleIcon1} alt="" src="rectangle-22@2x.png" />
+      <img className={styles.rectangleIcon2} alt="" src="rectangle-23@2x.png" />
+      <img className={styles.rectangleIcon3} alt="" src="rectangle-23@2x.png" />
+      <img className={styles.rectangleIcon4} alt="" src="rectangle-22@2x.png" />
+      <img className={styles.rectangleIcon5} alt="" src="rectangle-22@2x.png" />
+      <a className={styles.uIUXDesigningCourse}>UI/UX Designing Course</a>
+      <a className={styles.uIUXDesigningCourse1}>UI/UX Designing Course</a>
+      <a className={styles.uIUXDesigningCourse2}>UI/UX Designing Course</a>
+      <a className={styles.uIUXDesigningCourse3}>UI/UX Designing Course</a>
+      <a className={styles.uIUXDesigningCourse4}>UI/UX Designing Course</a>
+      <a className={styles.uIUXDesigningCourse5}>UI/UX Designing Course</a>
+      <a className={styles.rs699A}>Rs.699</a>
+      <a className={styles.rs699A1}>Rs.699</a>
+      <a className={styles.rs699A2}>Rs.699</a>
+      <a className={styles.rs699A3}>Rs.699</a>
+      <a className={styles.rs699A4}>Rs.699</a>
+      <a className={styles.rs699A5}>Rs.699</a>
+      <footer className={styles.rectangleFooter} />
+      <img className={styles.rectangleIcon6} alt="" src="rectangle-29@2x.png" />
+      <h4 className={styles.uIUXDesigningCourse6}>UI/UX Designing Course</h4>
+      <p className={styles.rs699P}>Rs.699</p>
+      <p className={styles.loremIpsumIsTheDummyText}>
+        <p className={styles.loremIpsumIs}>
+          Lorem ipsum is the dummy text used tokdkdi kdolll kolk
+        </p>
+        <p className={styles.yundjkdiIdkdldieDkdldiek}>
+          yundjkdi idkdldie dkdldiek idiekm in the nrooot .
+        </p>
+      </p>
+      <img className={styles.vectorIcon} alt="" src="vector.svg" />
+      <img className={styles.vectorIcon1} alt="" src="vector1.svg" />
+      <img className={styles.vectorIcon2} alt="" src="vector2.svg" />
+      <img className={styles.vectorIcon3} alt="" src="vector3.svg" />
+      <img className={styles.vectorIcon4} alt="" src="vector4.svg" />
+      <img className={styles.vectorIcon5} alt="" src="vector5.svg" />
+      <img className={styles.vectorIcon6} alt="" src="vector6.svg" />
+      <img className={styles.vectorIcon7} alt="" src="vector7.svg" />
+      <img className={styles.vectorIcon8} alt="" src="vector8.svg" />
+      <img className={styles.vectorIcon9} alt="" src="vector9.svg" />
+      <img className={styles.vectorIcon10} alt="" src="vector10.svg" />
+      <img className={styles.vectorIcon11} alt="" src="vector11.svg" />
+      <img className={styles.vectorIcon12} alt="" src="vector12.svg" />
+      <img className={styles.vectorIcon13} alt="" src="vector13.svg" />
+      <img className={styles.vectorIcon14} alt="" src="vector14.svg" />
+      <img className={styles.vectorIcon15} alt="" src="vector15.svg" />
+      <img className={styles.vectorIcon16} alt="" src="vector16.svg" />
+      <img className={styles.vectorIcon17} alt="" src="vector17.svg" />
+      <img className={styles.vectorIcon18} alt="" src="vector18.svg" />
+      <img className={styles.vectorIcon19} alt="" src="vector19.svg" />
+      <img className={styles.vectorIcon20} alt="" src="vector20.svg" />
+      <img className={styles.vectorIcon21} alt="" src="vector21.svg" />
+      <img className={styles.vectorIcon22} alt="" src="vector22.svg" />
+      <img className={styles.vectorIcon23} alt="" src="vector23.svg" />
+      <img className={styles.vectorIcon24} alt="" src="vector24.svg" />
+      <img className={styles.vectorIcon25} alt="" src="vector25.svg" />
+      <img className={styles.vectorIcon26} alt="" src="vector26.svg" />
+    </div>
+  );
+};
